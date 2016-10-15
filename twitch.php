51130efb74ddb9533efb948cd987c07e9e8108d7
Giuseppe <toxiicpp@gmail.com>
2016-10-15 17:56:47 +0200

    Update twitch.php

--- twitch.php
+++ twitch.php
@@ -9,7 +9,7 @@ function Curl($url, $header)
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,3);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 1);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 	$response = curl_exec($ch);
