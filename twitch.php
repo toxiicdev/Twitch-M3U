d5614e00e5da29fbdeacfc09b10ae5cc688ef8cb
Giuseppe <admin@toxiicdev.net>
2016-10-20 23:15:27 +0200

    Added comments
    Added comments

--- twitch.php
+++ twitch.php
@@ -4,6 +4,8 @@
 
 $clientId = "your_client_key_here"; // Set your client key
 
+// Curl function
+
 function Curl($url, $header)
 {
 	$ch = curl_init();
@@ -17,8 +19,11 @@ function Curl($url, $header)
 	return $response;
 }
 
+// Check for get parameter 'channel' to exist and to be valid
+
 if (isset($_GET['channel']) && preg_match("/^[a-zA-Z0-9_]{4,25}$/u", $_GET['channel']))
 {
+	// Lets request to twitch tv the access token
 	$response = Curl("https://api.twitch.tv/api/channels/" . $_GET['channel'] . "/access_token/",
 				[
 					"Client-ID: $clientId",
@@ -28,18 +33,21 @@ if (isset($_GET['channel']) && preg_match("/^[a-zA-Z0-9_]{4,25}$/u", $_GET['chan
 				]
 			);
 	
-	if(strstr($response, "token")) // There's the token!
+	// If the reply contains token
+	if(strstr($response, "token"))
 	{
-		$json = json_decode($response, true);
+		$json = json_decode($response, true); // Decode the JSON array
 		header('Location: http://usher.twitch.tv/api/channel/hls/' . $_GET['channel'] . '.m3u8?player=twitchweb&token=' . rawurlencode($json['token']) . '&sig=' . $json['sig']);
 	}
 	else
 	{
+		// Print error
 		echo "Couldn't get the token";
 	}
 }
 else
 {
+	// Print error
 	echo "Channel name not provided or invalid";
 }
 
